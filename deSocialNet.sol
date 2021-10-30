61d38c4187b3f723d75e17aaf4a658e14343b879
TheGreatAlgo <37487508+TheGreatAlgo@users.noreply.github.com>
2021-10-30 13:44:47 -0400

    Add files via upload

--- deSocialNet.sol
+++ deSocialNet.sol
@@ -3,46 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
-//import "https://github.com/sushiswap/sushiswap/blob/canary/contracts/uniswapv2/UniswapV2Router02.sol";
-
-interface ISushiswapV2Router02 {
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-        function WETH() external pure returns (address);
-
-}
-
-contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
 
 
-    ISushiswapV2Router02 sushi = ISushiswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
 
+contract deSocialNet is KeeperCompatibleInterface, Ownable {
 
-    bytes32 internal keyHash;
-    uint256 internal fee;
-
-    uint256 public randomResult;
-
-    //address WETHKovan = 0x2583407163B7F3F52f42d427F8634a7A652DC311;
-    address LINKKovan = 0xa36085F69e2889c224210F603D836748e7dC0088;
-
-    constructor()
-        VRFConsumerBase(
-            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
-            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
-        )
-    {
-        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
-        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
-    }
-
+    uint256 public lastCheckIn = block.timestamp;
+    uint256 public checkInTimeInterval = 864000; //default to six months
+    address public nextOwner;
 
     struct Post
     {
@@ -67,25 +36,34 @@ contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
         string userProfileBio;
         string userNickname;
         uint256 followerCount;
-        uint256[] userPosts; // list of answer keys so we can look them up
-        mapping(uint256 => Post) postStructs; // random access by question key and answer key
-        // add more non-key fields as needed
+        uint256 joinDate;
+        uint256 featuredPost;
+        uint256[] userPosts; // list of userPosts. probably can remove
+        mapping(uint256 => Post) postStructs; // mapping of postkey to post
     }
 
-    mapping(address => userProfile) userProfileStructs; // random access by question key
+    mapping(address => userProfile) userProfileStructs; // mapping useraddress to user profile
     address[] userProfileList; // list of user profiles
-    mapping(bytes32 => postRequest) postRequestStruct; //Used for the Async Call
+    //mapping(bytes32 => postRequest) postRequestStruct; //Used for the Async Keeper Call
+    event sendMessageEvent(address senderAddress, address recipientAddress, uint256 time, string message);
 
-    function newProfile(string memory text, string memory nickName )
+    function sendMessage(address recipientAddress, string memory message) public {
+        require(userProfileStructs[msg.sender].exists == true, "Create an Account to Post"); // Check to see if they have an account
+        emit sendMessageEvent(msg.sender, recipientAddress,block.timestamp, message);
+    }
+
+    function newProfile(string memory newProfileBio, string memory nickName )
     public
         // onlyOwner
         returns(bool success)
     {
         require(userProfileStructs[msg.sender].exists == false, "Account Already Created"); // Check to see if they have an account
-        userProfileStructs[msg.sender].userProfileBio = text;
+        userProfileStructs[msg.sender].userProfileBio = newProfileBio;
         userProfileStructs[msg.sender].userNickname = nickName;
         userProfileStructs[msg.sender].followerCount = 0;
         userProfileStructs[msg.sender].exists = true;
+        userProfileStructs[msg.sender].joinDate = block.timestamp;
+        userProfileStructs[msg.sender].featuredPost = 0;
         userProfileList.push(msg.sender);
         return true;
     }
@@ -93,44 +71,75 @@ contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
     function getUserProfile(address userAddress)
         public
         view
-        returns(string memory profileBio, uint totalPosts)
+        returns(string memory profileBio, uint256 totalPosts,uint256 joinDate,uint256 followerCount, string memory userNickname, uint256 featuredPost)
     {
-        return(userProfileStructs[userAddress].userProfileBio, userProfileStructs[userAddress].userPosts.length);
+        return(userProfileStructs[userAddress].userProfileBio,
+        userProfileStructs[userAddress].userPosts.length,
+        userProfileStructs[userAddress].joinDate,
+        userProfileStructs[userAddress].followerCount,
+        userProfileStructs[userAddress].userNickname,
+        userProfileStructs[userAddress].featuredPost);
     }
 
-    // function addPost(string memory messageText, uint256 randomResult1)
-    //     public
-    //     // onlyOwner
-    //     returns(bool success)
-    // {
-    //     // call chainlink VRF for postKey
-    //     // Check that they exist
-    //     userProfileStructs[msg.sender].userPosts.push(randomResult1);
-    //     userProfileStructs[msg.sender].postStructs[randomResult1].message = messageText;
-    //     userProfileStructs[msg.sender].postStructs[randomResult1].timestamp = block.timestamp;
-    //     userProfileStructs[msg.sender].postStructs[randomResult1].numberOfLikes = 0;
-    //     // answer vote will init to 0 without our help
-    //     return true;
-    // }
+     function addPost(string memory messageText, string memory url)
+         public
+         returns(bool success)
+     {
+         require(userProfileStructs[msg.sender].exists == true, "Create an Account to Post"); // Check to see if they have an account
+         uint256 postID = (userProfileStructs[msg.sender].userPosts.length); // ID is just an increment. No need to be random since it is associated to each unique account
+         userProfileStructs[msg.sender].userPosts.push(postID);
+         userProfileStructs[msg.sender].postStructs[postID].message = messageText;
+         userProfileStructs[msg.sender].postStructs[postID].timestamp = block.timestamp;
+         userProfileStructs[msg.sender].postStructs[postID].numberOfLikes = 0;
+         userProfileStructs[msg.sender].postStructs[postID].url = url;
+         return true;
+     }
+
+    function changeUserBio(string memory bioText)
+         public
+         returns(bool success)
+     {
+         require(userProfileStructs[msg.sender].exists == true, "Create an Account First"); // Check to see if they have an account
+         userProfileStructs[msg.sender].userProfileBio = bioText ;
+         return true;
+     }
+
+    function changeUserNickname(string memory newNickName)
+         public
+         returns(bool success)
+     {
+         require(userProfileStructs[msg.sender].exists == true, "Create an Account First"); // Check to see if they have an account
+         userProfileStructs[msg.sender].userNickname = newNickName ;
+         return true;
+     }
+    function changeFeaturedPost(uint256 postNumber)
+         public
+         returns(bool success)
+     {
+         require(userProfileStructs[msg.sender].exists == true, "Create an Account First"); // Check to see if they have an account
+         userProfileStructs[msg.sender].featuredPost = postNumber ;
+         return true;
+     }
 
     function getUserPost(address userAddress, uint256 postKey)
         public
         view
-        returns(string memory message, uint256 numberOfLikes, uint256 timestamp, string memory url )
+        returns(string memory message, uint256 numberOfLikes, uint256 timestamp, string memory url, string memory userNickname )
     {
         return(
             userProfileStructs[userAddress].postStructs[postKey].message,
             userProfileStructs[userAddress].postStructs[postKey].numberOfLikes,
             userProfileStructs[userAddress].postStructs[postKey].timestamp,
-            userProfileStructs[userAddress].postStructs[postKey].url);
+            userProfileStructs[userAddress].postStructs[postKey].url,
+            userProfileStructs[userAddress].userNickname);
     }
 
         function getAllUserPosts(address userAddress)
         public
         view
-        returns(uint256[] memory userPosts)
+        returns(uint256 userPosts)
     {
-        return( userProfileStructs[userAddress].userPosts);
+        return( userProfileStructs[userAddress].userPosts.length);
     }
 
     function getTotalUsers()
@@ -144,6 +153,7 @@ contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
         public
         returns(bool success)
     {
+        require(userProfileStructs[msg.sender].exists == true, "Create an Account First"); // Check to see if they have an account
         userProfileStructs[userAddress].postStructs[postKey].numberOfLikes += 1;
         return true;
     }
@@ -151,6 +161,7 @@ contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
         public
         returns(bool success)
     {
+        require(userProfileStructs[msg.sender].exists == true, "Create an Account First"); // Check to see if they have an account
         userProfileStructs[userAddress].followerCount += 1;
         return true;
     }
@@ -158,90 +169,46 @@ contract SocialMedia is VRFConsumerBase, KeeperCompatibleInterface, Ownable {
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
-    function getLinkBalance() public view returns (uint) {
-        return LINK.balanceOf(address(this));
+
+    function changeInheritance(address newInheritor) public onlyOwner {
+        nextOwner = newInheritor;
+    }
+    function ownerCheckIn() public onlyOwner {
+        lastCheckIn = block.timestamp;
+    }
+    function changeCheckInTime(uint256 newCheckInTimeInterval) public onlyOwner {
+        checkInTimeInterval = newCheckInTimeInterval; // let owner change check in case he know he will be away for a while.
     }
 
-   // function donate(address _receiver) public payable {
-
-   // _receiver.call.value(msg.value).gas(20317)();
-//    }
-
-
-    function getRandomNumberAndPost(string memory messageText, string memory url) public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-        bytes32 tempRequestId = requestRandomness(keyHash, fee);
-        postRequestStruct[tempRequestId].userAddress = msg.sender; //temporarily stores the data using mapping since this will be an async call
-        postRequestStruct[tempRequestId].message = messageText; //temporarily stores the data using mapping since this will be an async call
-        postRequestStruct[tempRequestId].message = messageText; //temporarily stores the data using mapping since this will be an async call
-        postRequestStruct[tempRequestId].url= url; //temporarily stores the data using mapping since this will be an async call
-        return tempRequestId;
-    }
-
-    /**
-     * Callback function used by VRF Coordinator
-     */
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
-        userProfileStructs[postRequestStruct[requestId].userAddress].userPosts.push(randomness); //uses the stored data based on the requestID
-        userProfileStructs[postRequestStruct[requestId].userAddress].postStructs[randomness].message = postRequestStruct[requestId].message; //uses the stored data based on the requestID
-        userProfileStructs[postRequestStruct[requestId].userAddress].postStructs[randomness].timestamp = postRequestStruct[requestId].timestamp; //uses the stored data based on the requestID
-        userProfileStructs[postRequestStruct[requestId].userAddress].postStructs[randomness].numberOfLikes = 0; //uses the stored data based on the requestID
-        userProfileStructs[postRequestStruct[requestId].userAddress].postStructs[randomness].url = postRequestStruct[requestId].url; //uses the stored data based on the requestID
+    function passDownInheritance() internal {
+        transferOwnership( nextOwner);
     }
 
     function checkUpkeep(bytes calldata /* checkData */) external override returns (bool upkeepNeeded, bytes memory /* performData */) {
-        upkeepNeeded = LINK.balanceOf(address(this)) < fee*100; // makesure enough link is there
+        //upkeepNeeded = (block.timestamp > (lastCheck + 5184000));
         // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
-        return (upkeepNeeded, bytes(""));
+        return (block.timestamp > (lastCheckIn + checkInTimeInterval), bytes("")); // make sure to check in at least once every 6 months
     }
 
     function performUpkeep(bytes calldata /* performData */) external override {
-        swapEthforLink();
-        // We don't use the performData in this example. The performData is generated by the Keeper's call to your checkUpkeep function
+        passDownInheritance();
     }
 
-    function swapEthforLink() public payable {
-        uint256 amount = (address(this).balance / 100) * 90 ;
-        address[] memory path = new address[](2);
-        path[0] = sushi.WETH();
-        path[1] = LINKKovan;
-        uint256 amountOutMin = 1.0 * 10 ** 18; // minimum 1 link
-        sushi.swapExactTokensForTokens(amount,amountOutMin, path, address(this), (block.timestamp+120)); //require at least 10 link tokens, send to this address, timelimit is 120 seconds.
+    function withdraw(uint amount) public onlyOwner returns(bool) {
+        require(amount <= address(this).balance);
+        payable(msg.sender).transfer(amount); //if the owner send to sender
+        return true;
     }
 
-    function withdraw(uint256 _amount) public payable onlyOwner {
-    payable(msg.sender).transfer(_amount);
-    }
-
-    function withdrawErc20(IERC20 token) public payable onlyOwner{
+    function withdrawErc20(IERC20 token) public onlyOwner{
       require(token.transfer(msg.sender, token.balanceOf(address(this))), "Transfer failed");
     }
+
+
 
     fallback() external payable {
         // nothing to do
     }
 
-
-    // function transfer(address to, uint value)
-    // returns(bool success)
-    // {
-    //   if(balances[msg.sender] < value) throw;
-    //   balances[msg.sender] -= value;
-    //   fee = value*0.1;
-    //   newValue = value*0.9;
-    //   to.transfer(newValue);
-    //   this.owner.transfer(fee); //Collect fee to pay Chainlink Oracles
-    //   LogTransfer(msg.sender, to, value);
-    //   return true;
-    // }
-    //
-    // function donate(address userAddress, bytes32 postKey)
-    //     public
-    //     constant
-    //     returns(bool success)
-    // {
-    //     userProfileStructs[userAddress].postStructs[postKey].numberOfLikes += 1;
-    //     return true;
-    // }
 
 }
---
+++ deSocialNetOnlineStore.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+
+contract onlineStore is KeeperCompatibleInterface, Ownable {
+
+    IERC20 deSocialNetToken = IERC20(0x25fE2b4d03dF8144D12F09Be547732e57752C29C);   //adress of desocialNet token
+
+    uint256 public lastCheckIn = block.timestamp;
+    uint256 public checkInTimeInterval = 864000; //default to six months
+    address public nextOwner;
+
+    uint256 public massivePurchaseTokenPrice =0.001*10**18; // 1 Million tokens is 1 Ether
+    uint256 public largePurchaseTokenPrice = 0.00015*10**18; // 100,000 tokens is 0.15 Ether
+    uint256 public mediumPurchaseTokenPrice = 0.00004*10**18; // 20,0000 tokens is 0.04 Ether
+    uint256 public smallPurchaseTokenPrice = 0.000025*10**18; // 10,0000 tokens is 0.025 Ether
+
+
+
+    function buyMassiveTokens() public payable{ // how many tokens they want to purchase
+        require(deSocialNetToken.balanceOf(address(this)) >= 1000000*10**10,"Not Enought Tokens in Contract"); // require this contract to have at least 1,000,000 tokens before executing
+        require(msg.value == massivePurchaseTokenPrice,"Send the right amount of eth"); // require this contract to have at least 1,000,000 tokens before executing
+         deSocialNetToken.transfer(msg.sender, 1000000*10**18); // send a million tokens.
+    }
+
+    function buyLargeTokens() public payable{ // how many tokens they want to purchase
+        require(deSocialNetToken.balanceOf(address(this)) >= 100000*10**10,"Not Enought Tokens in Contract"); // require this contract to have at least 1,000,000 tokens before executing
+        require(msg.value == largePurchaseTokenPrice,"Send the right amount of eth"); // require this contract to have at least 1,000,000 tokens before executing
+        deSocialNetToken.transfer(msg.sender, 100000*10**18); // send 100,0000 tokens.
+    }
+    function buyMediumTokens() public payable{ // how many tokens they want to purchase
+        require(deSocialNetToken.balanceOf(address(this)) >= 20000*10**10,"Not Enought Tokens in Contract"); // require this contract to have at least 1,000,000 tokens before executing
+        require(msg.value == mediumPurchaseTokenPrice,"Send the right amount of eth"); // require this contract to have at least 1,000,000 tokens before executing
+        deSocialNetToken.transfer(msg.sender, 20000*10**18); // send 20,0000 tokens.
+    }
+    function buySmallTokens() public payable{ // how many tokens they want to purchase
+        require(deSocialNetToken.balanceOf(address(this)) >= 10000*10**10,"Not Enought Tokens in Contract"); // require this contract to have at least 1,000,000 tokens before executing
+        require(msg.value == smallPurchaseTokenPrice,"Send the right amount of eth"); // require this contract to have at least 1,000,000 tokens before executing
+        deSocialNetToken.transfer(msg.sender, 10000*10**18); // send 10,0000 tokens.
+
+    }
+
+
+    function withdrawErc20(IERC20 token) public onlyOwner{ //withdraw all ERC-20 that get accidently sent since this is an only ether store.
+      require(token.transfer(msg.sender, token.balanceOf(address(this))), "Transfer failed");
+    }
+
+
+    function withdraw(uint amount) public onlyOwner returns(bool) {
+        require(amount <= address(this).balance);
+        payable(msg.sender).transfer(amount); //if the owner send to sender
+        return true;
+    }
+
+    function setMassiveStorePrice(uint256 newPrice) public onlyOwner {
+        require (newPrice <= massivePurchaseTokenPrice*2, "too high price"); // just in case you fat finger a number and accidently set a number too high or too low
+        require (newPrice >= massivePurchaseTokenPrice/2, "too low price");
+        massivePurchaseTokenPrice = newPrice;
+    }
+    function setLargeStorePrice(uint256 newPrice) public onlyOwner {
+        require (newPrice <= largePurchaseTokenPrice*2, "too high price"); // just in case you fat finger a number and accidently set a number too high or too low
+        require (newPrice >= largePurchaseTokenPrice/2, "too low price");
+        largePurchaseTokenPrice = newPrice;
+    }
+    function setMediumStorePrice(uint256 newPrice) public onlyOwner {
+        require (newPrice <= mediumPurchaseTokenPrice*2, "too high price"); // just in case you fat finger a number and accidently set a number too high or too low
+        require (newPrice >= mediumPurchaseTokenPrice/2, "too low price");
+        mediumPurchaseTokenPrice = newPrice;
+    }
+    function setsmallStorePrice(uint256 newPrice) public onlyOwner {
+        require (newPrice <= smallPurchaseTokenPrice*2, "too high price"); // just in case you fat finger a number and accidently set a number too high or too low
+        require (newPrice >= smallPurchaseTokenPrice/2, "too low price");
+        smallPurchaseTokenPrice = newPrice;
+    }
+
+    function changeInheritance(address newInheritor) public onlyOwner {
+        nextOwner = newInheritor;
+    }
+    function ownerCheckIn() public onlyOwner {
+        lastCheckIn = block.timestamp;
+    }
+    function changeCheckInTime(uint256 newCheckInTimeInterval) public onlyOwner {
+        checkInTimeInterval = newCheckInTimeInterval; // let owner change check in case he know he will be away for a while.
+    }
+
+    function passDownInheritance() internal {
+        transferOwnership( nextOwner);
+    }
+
+    function checkUpkeep(bytes calldata /* checkData */) external override returns (bool upkeepNeeded, bytes memory /* performData */) {
+    //upkeepNeeded = (block.timestamp > (lastCheck + 5184000));
+    // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+    return (block.timestamp > (lastCheckIn + checkInTimeInterval), bytes("")); // make sure to check in at least once every 6 months
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external override {
+       passDownInheritance();
+    }
+
+
+
+
+}
